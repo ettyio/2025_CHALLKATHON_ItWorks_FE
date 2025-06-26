da2b3e9f4aec92770550e0fe89a31ed1930151fc
seojeong <sjeong11ee@gmail.com>
2025-06-26 21:23:22 +0900

    vite.config.js update

--- Reletter/src/api/axios.js
+++ Reletter/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api', // 백엔드 주소
+  baseURL: 'https://two025-challkathon-itworks-be-xrja.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
